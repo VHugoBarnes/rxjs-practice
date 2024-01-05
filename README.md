fa469a3503e89ebb36ca7c28ddede10a0739fc3e
Victor Hugo Vazquez Gomez <hugovg799@gmail.com>
2024-01-04 20:32:27 -0600

    One observable for letters and other for Backspace

---
+++ README.md
@@ -0,0 +1,7 @@
+# Wordle clone
+This is a Wordle clone using RxJs with TypeScript.   
+
+# Installation
+1. Install `pnpm`.   
+2. Perform `pnpm install`.   
+3. Perform `pnpm dev`.   
--- src/main.ts
+++ src/main.ts
@@ -1,15 +1,39 @@
-import { Subscription, fromEvent } from "rxjs";
+import "./style.css";
+import { Observable, Observer, fromEvent } from "rxjs";
 
-const onMouseMove$ = fromEvent(document, "keydown");
+const letterRows = document.getElementsByClassName("letter-row");
 
-const subscription: Subscription = onMouseMove$.subscribe({
-  next(value) {
-    console.log(value);
+const onKeyDown$: Observable<KeyboardEvent> = fromEvent<KeyboardEvent>(document, "keydown");
+let letterIndex = 0;
+let letterRowIndex = 0;
+const insertLetter: Observer<KeyboardEvent> = {
+  next(event) {
+    const pressedKey = event.key.toUpperCase();
+    if (pressedKey.length === 1 && pressedKey.match(/[a-z]/i)) {
+      let letterBox = Array.from(letterRows)[letterRowIndex].children[letterIndex];
+      letterBox.textContent = pressedKey;
+      letterBox.classList.add("filled-letter");
+      letterIndex++;
+    }
+
   },
   complete() {
 
   },
   error(err) {
+    console.error(err);
+  },
+}
+onKeyDown$.subscribe(insertLetter);
 
+const onDeleteKey$: Observable<KeyboardEvent> = fromEvent<KeyboardEvent>(document, "keydown");
+onDeleteKey$.subscribe({
+  next(event) {
+    if (event.key === "Backspace") {
+      letterIndex--;
+      const letterBox = Array.from(letterRows)[letterRowIndex].children[letterIndex];
+      letterBox.textContent = " ";
+      letterBox.classList.remove("filled-letter");
+    }
   },
 });
