23a852465981cd1e658996a5699bf095ab31153b
Victor Hugo Vazquez Gomez <hugovg799@gmail.com>
2024-01-05 14:04:50 -0600

    applying subjects

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,19 @@
 import "./style.css";
-import { Observable, Observer, fromEvent } from "rxjs";
+import { Observable, Observer, Subject, fromEvent } from "rxjs";
+import WORDS_LIST from "./wordsList.json";
 
 const letterRows = document.getElementsByClassName("letter-row");
+
+const userWinOrLoss = new Subject();
 
 const onKeyDown$: Observable<KeyboardEvent> = fromEvent<KeyboardEvent>(document, "keydown");
 let letterIndex = 0;
 let letterRowIndex = 0;
+let userAnswer: string[] = [];
+const getRandomWord = () => WORDS_LIST[Math.floor(Math.random() * WORDS_LIST.length)];
+const rightWord = getRandomWord();
+console.log(rightWord);
+
 const insertLetter: Observer<KeyboardEvent> = {
   next(event) {
     const pressedKey = event.key.toUpperCase();
@@ -13,6 +21,7 @@ const insertLetter: Observer<KeyboardEvent> = {
       let letterBox = Array.from(letterRows)[letterRowIndex].children[letterIndex];
       letterBox.textContent = pressedKey;
       letterBox.classList.add("filled-letter");
+      userAnswer.push(pressedKey);
       letterIndex++;
     }
 
@@ -26,6 +35,23 @@ const insertLetter: Observer<KeyboardEvent> = {
 }
 onKeyDown$.subscribe(insertLetter);
 
+const checkWord: Observer<KeyboardEvent> = {
+  next(event) {
+    if (event.key === "Enter") {
+      if (rightWord === userAnswer.join("")) {
+        userWinOrLoss.next("win");
+      }
+    }
+  },
+  complete() {
+
+  },
+  error(err) {
+    console.error(err);
+  },
+};
+onKeyDown$.subscribe(checkWord);
+
 const onDeleteKey$: Observable<KeyboardEvent> = fromEvent<KeyboardEvent>(document, "keydown");
 onDeleteKey$.subscribe({
   next(event) {
@@ -36,4 +62,12 @@ onDeleteKey$.subscribe({
       letterBox.classList.remove("filled-letter");
     }
   },
+});
+userWinOrLoss.subscribe(() => {
+  let letterRowsWinned = Array.from(letterRows)[letterRowIndex];
+  console.log(letterRowsWinned);
+
+  for (let i = 0; i < 5; i++) {
+    letterRowsWinned.children[i].classList.add("letter-green");
+  }
 });
